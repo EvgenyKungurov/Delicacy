36354b4c6840ed122c5c362901eb71cfd563002c
EvgenyKungurov <evgenii.kungurov@gmail.com>
2017-06-11 07:42:23 +0000

    Add validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
+  include AwesomeNestedSetMethods
+
+  validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
+  has_many :items
   acts_as_nested_set
+
+  before_save   :name_capitalize
+  after_create  :parent_count_increase
+  after_destroy :parent_count_decrease
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :category
+  validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
 end
