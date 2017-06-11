7461b9ee8e4aa0336eb7338399bb5851275b420c
EvgenyKungurov <evgenii.kungurov@gmail.com>
2017-06-11 08:00:06 +0000

    Add category presense validate

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Item, type: :model do
   subject { FactoryGirl.build(:item, category_id: category.id) }
 
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:category_id) }
   it { should validate_length_of(:name).is_at_least 3 }
   it { should belong_to(:category) }
 end
